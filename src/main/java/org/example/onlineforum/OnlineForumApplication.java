30185364ac2f47d635b592b0cf480062be0269d3
vertunoff <anton.s.vertunov@gmail.com>
2024-10-27 02:19:39 +0300

    project initialization

---
+++ src/main/java/org/example/onlineforum/OnlineForumApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlineforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/onlineforum/OnlineForumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlineforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
